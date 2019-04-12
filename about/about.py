f734d8282814cc489cf638361e47e20b3a7292fb
Austin Ryan <37423484+austinryan@users.noreply.github.com>
2019-04-12 15:17:20 -0400

    update about window
    - added icon
    - changed anything related to videoautosplitter to AutoSplit

--- about/about.py
+++ about/about.py
@@ -22,44 +22,56 @@ except AttributeError:
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig)
 
-class Ui_aboutVideoAutoSplitterWidget(object):
-    def setupUi(self, aboutVideoAutoSplitterWidget):
-        aboutVideoAutoSplitterWidget.setObjectName(_fromUtf8("aboutVideoAutoSplitterWidget"))
-        aboutVideoAutoSplitterWidget.resize(276, 249)
-        aboutVideoAutoSplitterWidget.setMinimumSize(QtCore.QSize(276, 249))
-        aboutVideoAutoSplitterWidget.setMaximumSize(QtCore.QSize(276, 249))
-        self.okButton = QtGui.QPushButton(aboutVideoAutoSplitterWidget)
-        self.okButton.setGeometry(QtCore.QRect(200, 220, 71, 21))
+class Ui_aboutAutoSplitWidget(object):
+    def setupUi(self, aboutAutoSplitWidget):
+        aboutAutoSplitWidget.setObjectName(_fromUtf8("aboutAutoSplitWidget"))
+        aboutAutoSplitWidget.resize(276, 249)
+        aboutAutoSplitWidget.setMinimumSize(QtCore.QSize(276, 249))
+        aboutAutoSplitWidget.setMaximumSize(QtCore.QSize(276, 249))
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(_fromUtf8(":/resources/icon.ico")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        aboutAutoSplitWidget.setWindowIcon(icon)
+        self.okButton = QtGui.QPushButton(aboutAutoSplitWidget)
+        self.okButton.setGeometry(QtCore.QRect(190, 220, 71, 21))
         self.okButton.setObjectName(_fromUtf8("okButton"))
-        self.createdbyLabel = QtGui.QLabel(aboutVideoAutoSplitterWidget)
-        self.createdbyLabel.setGeometry(QtCore.QRect(10, 33, 151, 16))
+        self.createdbyLabel = QtGui.QLabel(aboutAutoSplitWidget)
+        self.createdbyLabel.setGeometry(QtCore.QRect(10, 44, 151, 16))
         self.createdbyLabel.setObjectName(_fromUtf8("createdbyLabel"))
-        self.versionLabel = QtGui.QLabel(aboutVideoAutoSplitterWidget)
-        self.versionLabel.setGeometry(QtCore.QRect(10, 10, 71, 16))
+        self.versionLabel = QtGui.QLabel(aboutAutoSplitWidget)
+        self.versionLabel.setGeometry(QtCore.QRect(10, 21, 71, 16))
         self.versionLabel.setObjectName(_fromUtf8("versionLabel"))
-        self.donatetextLabel = QtGui.QLabel(aboutVideoAutoSplitterWidget)
-        self.donatetextLabel.setGeometry(QtCore.QRect(48, 86, 191, 41))
+        self.donatetextLabel = QtGui.QLabel(aboutAutoSplitWidget)
+        self.donatetextLabel.setGeometry(QtCore.QRect(46, 95, 191, 41))
         self.donatetextLabel.setObjectName(_fromUtf8("donatetextLabel"))
-        self.donatebuttonLabel = QtGui.QLabel(aboutVideoAutoSplitterWidget)
+        self.donatebuttonLabel = QtGui.QLabel(aboutAutoSplitWidget)
         self.donatebuttonLabel.setGeometry(QtCore.QRect(52, 127, 171, 91))
         self.donatebuttonLabel.setAlignment(QtCore.Qt.AlignCenter)
         self.donatebuttonLabel.setObjectName(_fromUtf8("donatebuttonLabel"))
-        self.githubLabel = QtGui.QLabel(aboutVideoAutoSplitterWidget)
-        self.githubLabel.setGeometry(QtCore.QRect(10, 60, 46, 13))
-        self.githubLabel.setObjectName(_fromUtf8("githubLabel"))
+        self.iconLabel = QtGui.QLabel(aboutAutoSplitWidget)
+        self.iconLabel.setGeometry(QtCore.QRect(190, 17, 62, 71))
+        self.iconLabel.setObjectName(_fromUtf8("iconLabel"))
 
-        self.retranslateUi(aboutVideoAutoSplitterWidget)
-        QtCore.QObject.connect(self.okButton, QtCore.SIGNAL(_fromUtf8("clicked()")), aboutVideoAutoSplitterWidget.close)
-        QtCore.QMetaObject.connectSlotsByName(aboutVideoAutoSplitterWidget)
+        self.retranslateUi(aboutAutoSplitWidget)
+        QtCore.QObject.connect(self.okButton, QtCore.SIGNAL(_fromUtf8("clicked()")), aboutAutoSplitWidget.close)
+        QtCore.QMetaObject.connectSlotsByName(aboutAutoSplitWidget)
 
-    def retranslateUi(self, aboutVideoAutoSplitterWidget):
-        aboutVideoAutoSplitterWidget.setWindowTitle(_translate("aboutVideoAutoSplitterWidget", "About Video Auto Splitter", None))
-        self.okButton.setText(_translate("aboutVideoAutoSplitterWidget", "OK", None))
-        self.createdbyLabel.setText(_translate("aboutVideoAutoSplitterWidget", "<html><head/><body><p>Created by <a href=\"https://twitter.com/toufool\"><span style=\" text-decoration: underline; color:#0000ff;\">Toufool</span></a> and <a href=\"https://twitter.com/faschz\"><span style=\" text-decoration: underline; color:#0000ff;\">Faschz</span></a></p></body></html>", None))
-        self.versionLabel.setText(_translate("aboutVideoAutoSplitterWidget", "Version: 1.0.0", None))
-        self.donatetextLabel.setText(_translate("aboutVideoAutoSplitterWidget", "If you enjoy using this program, please\n"
-"      consider donating. Thank you!", None))
-        self.donatebuttonLabel.setText(_translate("aboutVideoAutoSplitterWidget", "<html><head/><body><p><a href=\"https://www.paypal.com/cgi-bin/webscr?cmd=_donations&amp;business=BYRHQG69YRHBA&amp;item_name=Video+Auto+Splitter+Development&amp;currency_code=USD&amp;source=url\"><img src=\":/donateButton/PayPal-Donate-Button-PNG-Clipart.png\"/></a></p></body></html>", None))
-        self.githubLabel.setText(_translate("aboutVideoAutoSplitterWidget", "<html><head/><body><p><a href=\"https://github.com/austinryan/Video-Auto-Splitter\"><span style=\" text-decoration: underline; color:#0000ff;\">Github</span></a></p></body></html>", None))
+    def retranslateUi(self, aboutAutoSplitWidget):
+        aboutAutoSplitWidget.setWindowTitle(_translate("aboutAutoSplitWidget", "About AutoSplit", None))
+        self.okButton.setText(_translate("aboutAutoSplitWidget", "OK", None))
+        self.createdbyLabel.setText(_translate("aboutAutoSplitWidget", "<html><head/><body><p>Created by <a href=\"https://twitter.com/toufool\"><span style=\" text-decoration: underline; color:#0000ff;\">Toufool</span></a> and <a href=\"https://twitter.com/faschz\"><span style=\" text-decoration: underline; color:#0000ff;\">Faschz</span></a></p></body></html>", None))
+        self.versionLabel.setText(_translate("aboutAutoSplitWidget", "Version: 1.0.0", None))
+        self.donatetextLabel.setText(_translate("aboutAutoSplitWidget", "If you enjoy using this program, please\n"
+"       consider donating. Thank you!", None))
+        self.donatebuttonLabel.setText(_translate("aboutAutoSplitWidget", "<html><head/><body><p><a href=\"https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=BYRHQG69YRHBA&item_name=AutoSplit+development&currency_code=USD&source=url\"><img src=\":/resources/donatebutton.png\"/></p></body></html>", None))
+        self.iconLabel.setText(_translate("aboutAutoSplitWidget", "<html><head/><body><p><img src=\":/resources/icon.ico\"/></p></body></html>", None))
 
-import donatebutton_rc
+import resources_rc
+
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    aboutAutoSplitWidget = QtGui.QWidget()
+    ui = Ui_aboutAutoSplitWidget()
+    ui.setupUi(aboutAutoSplitWidget)
+    aboutAutoSplitWidget.show()
+    sys.exit(app.exec_())
