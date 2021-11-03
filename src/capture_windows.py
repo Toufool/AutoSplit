8690800723f0c004ffed6dbac06b0ca1709a1df6
Avasam <samuel.06@hotmail.com>
2021-11-02 22:23:46 -0400

    Hardware rendered window capture

--- src/capture_windows.py
+++ src/capture_windows.py
@@ -1,11 +1,15 @@
-import numpy
-import cv2
-
+from ctypes import windll
+from ctypes.wintypes import LONG, RECT
+from win32 import win32gui
+import numpy as np
 import win32ui
-import win32gui
 import win32con
 
-def capture_region(hwnd, rect):
+# This is an undocumented nFlag value for PrintWindow
+PW_RENDERFULLCONTENT = 0x00000002
+
+
+def capture_region(hwnd: int, rect: RECT):
     """
     Captures an image of the region for a window matching the given
     parameters of the bounding box
@@ -14,25 +18,27 @@ def capture_region(hwnd, rect):
     @param rect: The coordinates of the region
     @return: The image of the region in the window in BGRA format
     """
-    
-    width = rect.right - rect.left
-    height = rect.bottom - rect.top
-   
-    wDC = win32gui.GetWindowDC(hwnd)
-    dcObj = win32ui.CreateDCFromHandle(wDC)
-    cDC = dcObj.CreateCompatibleDC()
+
+    width: LONG = rect.right - rect.left
+    height: LONG = rect.bottom - rect.top
+
+    windowDC = win32gui.GetWindowDC(hwnd)
+    dcObject = win32ui.CreateDCFromHandle(windowDC)
+    compatibleDC = dcObject.CreateCompatibleDC()
     bmp = win32ui.CreateBitmap()
-    bmp.CreateCompatibleBitmap(dcObj, width, height)
-    cDC.SelectObject(bmp)
-    cDC.BitBlt((0, 0), (width, height), dcObj, (rect.left, rect.top), win32con.SRCCOPY)
-   
-    img = bmp.GetBitmapBits(True)
-    img = numpy.frombuffer(img, dtype='uint8')
+    bmp.CreateCompatibleBitmap(dcObject, width, height)
+    compatibleDC.SelectObject(bmp)
+    compatibleDC.BitBlt((0, 0), (width, height), dcObject, (rect.left, rect.top), win32con.SRCCOPY)
+
+    # Force render full content through PrintWindow. Workaround to capture hardware accelerated windows
+    windll.user32.PrintWindow(hwnd, dcObject.GetSafeHdc(), PW_RENDERFULLCONTENT)
+
+    img: np._BufferType = np.frombuffer(bmp.GetBitmapBits(True), dtype='uint8')
     img.shape = (height, width, 4)
- 
-    dcObj.DeleteDC()
-    cDC.DeleteDC()
-    win32gui.ReleaseDC(hwnd, wDC)
+
+    dcObject.DeleteDC()
+    compatibleDC.DeleteDC()
+    win32gui.ReleaseDC(hwnd, windowDC)
     win32gui.DeleteObject(bmp.GetHandle())
-    
+
     return img
