5fa2e479926a1556fe505bd22c8e48378842f297
Avasam <samuel.06@hotmail.com>
2021-11-11 01:16:18 -0500

    check before PrintWindow

--- src/capture_windows.py
+++ src/capture_windows.py
@@ -1,13 +1,14 @@
 from ctypes import windll
-from ctypes.wintypes import LONG, RECT
+from ctypes.wintypes import LONG, RECT, HBITMAP
+from typing import Dict
 from win32 import win32gui
 import numpy as np
 import win32ui
 import win32con
-import numpy as np
 
 # This is an undocumented nFlag value for PrintWindow
 PW_RENDERFULLCONTENT = 0x00000002
+accelerated_windows: Dict[int, bool] = {}
 
 
 def capture_region(hwnd: int, rect: RECT):
@@ -20,26 +21,42 @@ def capture_region(hwnd: int, rect: RECT):
     @return: The image of the region in the window in BGRA format
     """
 
+    is_accelerated_window = accelerated_windows.get(hwnd)
+
+    # The window type is not yet known, let's find out!
+    if is_accelerated_window is None:
+        # We need to get the image at least once to check if it's full black
+        image = __get_image(hwnd, rect, False)
+        # TODO check for first non-black pixel, no need to iterate through the whole image
+        is_accelerated_window = not np.count_nonzero(image)
+        accelerated_windows[hwnd] = is_accelerated_window
+        return __get_image(hwnd, rect, True) if is_accelerated_window else image
+
+    return __get_image(hwnd, rect, is_accelerated_window)
+
+
+def __get_image(hwnd: int, rect: RECT, printWindow=False):
     width: LONG = rect.right - rect.left
     height: LONG = rect.bottom - rect.top
-
-    windowDC = win32gui.GetWindowDC(hwnd)
+    windowDC: int = win32gui.GetWindowDC(hwnd)
     dcObject = win32ui.CreateDCFromHandle(windowDC)
+
+    # Causes a 10-15x performance drop. But allows recording hardware accelerated windows
+    if (printWindow):
+        windll.user32.PrintWindow(hwnd, dcObject.GetSafeHdc(), PW_RENDERFULLCONTENT)
+
     compatibleDC = dcObject.CreateCompatibleDC()
-    bmp = win32ui.CreateBitmap()
-    bmp.CreateCompatibleBitmap(dcObject, width, height)
-    compatibleDC.SelectObject(bmp)
+    bitmap: HBITMAP = win32ui.CreateBitmap()
+    bitmap.CreateCompatibleBitmap(dcObject, width, height)
+    compatibleDC.SelectObject(bitmap)
     compatibleDC.BitBlt((0, 0), (width, height), dcObject, (rect.left, rect.top), win32con.SRCCOPY)
 
-    # Force render full content through PrintWindow. Workaround to capture hardware accelerated windows
-    windll.user32.PrintWindow(hwnd, dcObject.GetSafeHdc(), PW_RENDERFULLCONTENT)
-
-    img: np._BufferType = np.frombuffer(bmp.GetBitmapBits(True), dtype='uint8')
-    img.shape = (height, width, 4)
+    image: np._BufferType = np.frombuffer(bitmap.GetBitmapBits(True), dtype='uint8')
+    image.shape = (height, width, 4)
 
     dcObject.DeleteDC()
     compatibleDC.DeleteDC()
     win32gui.ReleaseDC(hwnd, windowDC)
-    win32gui.DeleteObject(bmp.GetHandle())
+    win32gui.DeleteObject(bitmap.GetHandle())
 
-    return img
+    return image
