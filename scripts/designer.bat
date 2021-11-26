e53ab5371069a6141e5ee2aff1ffbb97b7928e4d
Avasam <samuel.06@hotmail.com>
2021-11-26 14:20:07 -0500

    Fixed a Windows 11 high-DPI pixel rounding issue

--- scripts/designer.bat
+++ scripts/designer.bat
@@ -1,7 +1,14 @@
 @ECHO OFF
 
 IF NOT DEFINED PYTHONPATH (
-  FOR /f "delims=" %%p in ('where python') do SET PYTHONPATH=%%p
+  setlocal EnableDelayedExpansion
+
+  SET n=0
+  FOR /f "delims=" %%p in ('where python') do (
+    SET pythonFiles[!n!]=%%p
+    SET /A n+=1
+  )
+  SET PYTHONPATH=!pythonFiles[0]!
 )
 
 START "Qt Designer" "%PYTHONPATH:~0,-11%\Lib\site-packages\qt6_applications\Qt\bin\designer.exe" "%~d0%~p0..\res\design.ui" "%~d0%~p0..\res\about.ui" "%~d0%~p0..\res\update_checker.ui"
