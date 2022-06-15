a4dda9dc64fbd2aade5dadc686c22e8bfaf01faf
Avasam <samuel.06@hotmail.com>
2022-06-15 01:39:04 -0400

    Updated versioning

--- scripts/install.bat
+++ scripts/install.bat
@@ -1,5 +1,5 @@
 py -m pip install wheel --upgrade
-py -m pip install -r "%~p0requirements.txt" --upgrade
+py -m pip install -r "%~p0requirements.txt"
 CALL "%~p0compile_resources.bat"
 CALL npm install -g pyright@latest
 CALL npm list -g pyright
