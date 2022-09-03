a017599620d1df7ccdeefba8cd9b2153f8bca25d
Avasam <samuel.06@hotmail.com>
2022-09-03 13:06:53 -0400

    Add D3DShot types from https://github.com/python/typeshed/pull/8652

---
+++ typings/d3dshot/capture_outputs/pytorch_capture_output.pyi
@@ -0,0 +1,17 @@
+import torch
+from _typeshed import Incomplete
+from d3dshot.capture_output import CaptureOutput as CaptureOutput
+from PIL.Image import Image
+from typing_extensions import TypeAlias
+
+Pointer: TypeAlias = Incomplete
+
+
+class PytorchCaptureOutput(CaptureOutput):
+    def __init__(self) -> None: ...
+
+    def process(
+        self, pointer: Pointer, size: int, width: int, height: int, region: tuple[int, int, int, int], rotation: int
+    ) -> torch.Tensor: ...
+    def to_pil(self, frame: torch.Tensor) -> Image: ...
+    def stack(self, frames: list[torch.Tensor], stack_dimension: int) -> torch.Tensor: ...
