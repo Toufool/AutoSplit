8c37565ace9c8005de64a9307d23e70932ca0494
Avasam <samuel.06@hotmail.com>
2022-09-03 15:13:03 -0400

    Doc + Setup update

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,8 +1,8 @@
 & "$PSScriptRoot/compile_resources.ps1"
-
 pyinstaller `
   --windowed `
   --onefile `
   --additional-hooks-dir=Pyinstaller/hooks `
   --icon=res/icon.ico `
+  --splash=res/splash.png `
   "$PSScriptRoot/../src/AutoSplit.py"
