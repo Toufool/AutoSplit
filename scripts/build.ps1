ca88e7e4eab048d9456dd8aef646ee2a977aedf0
Avasam <samuel.06@hotmail.com>
2022-10-21 16:00:53 -0400

    Revert "back to windowed"
    This reverts commit 9553271121685d4ea3ecff2afb0dcd22d792be56.

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,6 +1,7 @@
 & "$PSScriptRoot/compile_resources.ps1"
 pyinstaller `
   --onefile `
+  --windowed `
   --additional-hooks-dir=Pyinstaller/hooks `
   --icon=res/icon.ico `
   --splash=res/splash.png `
