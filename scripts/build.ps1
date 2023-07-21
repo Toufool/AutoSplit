2287470112728b3aedd83d9b53ed1f9a93cf96bc
Avasam <samuel.06@hotmail.com>
2023-07-21 16:10:46 -0400

    Reduce build size by ~540 KB Before: 134.832 MB After: 134.298 MB

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -5,6 +5,12 @@ $arguments = @(
   '--onefile',
   '--windowed',
   '--additional-hooks-dir=Pyinstaller/hooks',
+  # Optional packages installed by PyAutoGUI
+  '--exclude=pygetwindow',
+  '--exclude=pymsgbox',
+  '--exclude=pytweening',
+  # Used by imagehash.whash
+  '--exclude=pywt',
   '--icon=res/icon.ico',
   '--splash=res/splash.png')
 
