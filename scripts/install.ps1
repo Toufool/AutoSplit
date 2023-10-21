d963dbda7263997bd7f04522842ca6029b440a22
Avasam <samuel.06@hotmail.com>
2023-10-21 16:48:25 -0400

    Build on Python 3.12 (#257)

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -3,10 +3,16 @@ $dev = If ($Env:GITHUB_JOB -eq 'Build') { '' } Else { '-dev' }
 # Ensures installation tools are up to date. This also aliases pip to pip3 on MacOS.
 python -m pip install wheel pip setuptools --upgrade
 pip install -r "$PSScriptRoot/requirements$dev.txt" --upgrade
+# These libraries install extra requirements we don't want
+# Open suggestion for support in requirements files: https://github.com/pypa/pip/issues/9948 & https://github.com/pypa/pip/pull/10837
+# PyAutoGUI: We only use it for hotkeys
+# ImageHash: uneeded + broken on Python 3.12 PyWavelets install
+# scipy: needed for ImageHash
+pip install PyAutoGUI ImageHash scipy --no-deps --upgrade
 
 # Patch libraries so we don't have to install from git
 
-# Prevent pyautogui and pywinctl from setting Process DPI Awareness, which Qt tries to do then throws warnings about it.
+# Prevent PyAutoGUI and pywinctl from setting Process DPI Awareness, which Qt tries to do then throws warnings about it.
 # The unittest workaround significantly increases build time, boot time and build size with PyInstaller.
 # https://github.com/asweigart/pyautogui/issues/663#issuecomment-1296719464
 $libPath = python -c 'import pyautogui as _; print(_.__path__[0])'
@@ -18,8 +24,11 @@ $libPath = python -c 'import pymonctl as _; print(_.__path__[0])'
 $libPath = python -c 'import pywinbox as _; print(_.__path__[0])'
 (Get-Content "$libPath/_pywinbox_win.py").replace('ctypes.windll.shcore.SetProcessDpiAwareness(2)', 'pass') |
   Set-Content "$libPath/_pywinbox_win.py"
-# Uninstall optional dependencies that PyInstaller picks up
-python -m pip uninstall pyscreeze mouseinfo pyperclip -y
+# Uninstall optional dependencies if PyAutoGUI was installed outside this script
+# pyscreeze -> pyscreenshot -> mss deps call SetProcessDpiAwareness
+# pygetwindow, pymsgbox, pytweening, MouseInfo are picked up by PySide6
+# (also --exclude from build script, but more consistent with unfrozen run)
+python -m pip uninstall pyscreeze pyscreenshot mss pygetwindow pymsgbox pytweening MouseInfo -y
 
 
 # Don't compile resources on the Build CI job as it'll do so in build script
