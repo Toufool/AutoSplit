b70419a0032499278598477d4d9eb44d611c362f
Avasam <samuel.06@hotmail.com>
2024-08-18 13:14:15 -0400

    re-apply

--- src/AutoSplit.py
+++ src/AutoSplit.py
@@ -33,7 +33,6 @@ from typing import NoReturn
 
 import cv2
 from cv2.typing import MatLike
-from gen import about, design, settings, update_checker
 from psutil import process_iter
 from PySide6 import QtCore, QtGui
 from PySide6.QtTest import QTest
@@ -45,6 +44,7 @@ import user_profile
 from AutoControlledThread import AutoControlledThread
 from AutoSplitImage import START_KEYWORD, AutoSplitImage, ImageType
 from capture_method import CaptureMethodBase, CaptureMethodEnum
+from gen import about, design, settings, update_checker
 from hotkeys import (
     HOTKEYS,
     KEYBOARD_GROUPS_ISSUE,
