ff320a5dfce130996ec519f29cdef36fae89e2cc
Avasam <samuel.06@hotmail.com>
2025-02-02 21:54:52 -0500

    Migrate to uv

--- scripts/designer.ps1
+++ scripts/designer.ps1
@@ -1,13 +1,5 @@
-$python = $IsWindows ? 'python' : 'python3'
-$qt6_applications_import = 'import qt6_applications; print(qt6_applications.__path__[0])'
+$qt6_applications_path = uv run python -c 'import qt6_applications; print(qt6_applications.__path__[0])'
 
-$qt6_applications_path = &"$python" -c $qt6_applications_import
-if ($null -eq $qt6_applications_path) {
-  Write-Host 'Designer not found, installing qt6_applications'
-  &"$python" -m pip install qt6_applications
-}
-
-$qt6_applications_path = &"$python" -c $qt6_applications_import
 & "$qt6_applications_path/Qt/bin/designer" `
   "$PSScriptRoot/../res/design.ui" `
   "$PSScriptRoot/../res/about.ui" `
--- scripts/start.ps1
+++ scripts/start.ps1
@@ -1,4 +1,3 @@
 param ([string]$p1)
 & "$PSScriptRoot/compile_resources.ps1"
-$python = $IsWindows ? 'python' : 'python3'
-&"$python" "$PSScriptRoot/../src/AutoSplit.py" $p1
+uv run "$PSScriptRoot/../src/AutoSplit.py" $p1
