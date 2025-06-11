208770c24a008b4786281d1fab59d23a4aa5479d
Avasam <samuel.06@hotmail.com>
2025-06-11 18:00:08 -0400

    Add --no-sync to all embed `uv run` commands

--- scripts/lint.ps1
+++ scripts/lint.ps1
@@ -5,10 +5,10 @@ Set-Location "$PSScriptRoot/.."
 $exitCodes = 0
 
 Write-Host "`nRunning dprint fmt ..."
-uv run --active dprint fmt
+uv run --active --no-sync dprint fmt
 
 Write-Host "`nRunning Ruff check ..."
-uv run --active ruff check --fix
+uv run --active --no-sync ruff check --fix
 $exitCodes += $LastExitCode
 if ($LastExitCode -gt 0) {
   Write-Host "`Ruff failed ($LastExitCode)" -ForegroundColor Red
@@ -18,12 +18,12 @@ else {
 }
 
 Write-Host "`nRunning Ruff format ..."
-uv run --active ruff format
+uv run --active --no-sync --no-sync ruff format
 
-$pyrightVersion = $(uv run --active pyright --version).replace('pyright ', '')
+$pyrightVersion = $(uv run --active --no-sync pyright --version).replace('pyright ', '')
 Write-Host "`nRunning Pyright $pyrightVersion ..."
 $Env:PYRIGHT_PYTHON_FORCE_VERSION = $pyrightVersion
-uv run --active pyright src/
+uv run --active --no-sync pyright src/
 $exitCodes += $LastExitCode
 if ($LastExitCode -gt 0) {
   Write-Host "`Pyright failed ($LastExitCode)" -ForegroundColor Red
