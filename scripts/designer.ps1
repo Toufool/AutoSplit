05a72c89fbb601052d47a27e24cb59498e0a5ef8
Avasam <samuel.06@hotmail.com>
2026-06-15 11:22:26 -0400

    Bump minimum Ubuntu to 24.04+ for ARM64 + drop `qt6-applications` (#359)

--- scripts/designer.ps1
+++ scripts/designer.ps1
@@ -1,9 +1,3 @@
 #! /usr/bin/pwsh
 
-$qt6_applications_path = uv run --active python -c 'import qt6_applications; print(qt6_applications.__path__[0])'
-
-& "$qt6_applications_path/Qt/bin/designer" `
-  "$PSScriptRoot/../res/design.ui" `
-  "$PSScriptRoot/../res/about.ui" `
-  "$PSScriptRoot/../res/settings.ui" `
-  "$PSScriptRoot/../res/update_checker.ui"
+uv run --active pyside6-designer res/*.ui
